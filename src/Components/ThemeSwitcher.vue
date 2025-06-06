257da97ba6601c237721e607dca97f8d23e821c3
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2025-06-06 20:13:40 -0300

    Enhance `ThemeSwitcher` to support persistent color scheme with cookies. Update dependencies to include `universal-cookie`.

--- src/Components/ThemeSwitcher.vue
+++ src/Components/ThemeSwitcher.vue
@@ -3,17 +3,21 @@
         :faIcon="faIcon"
         :label="label"
         :next="next"
-        :state="store"
+        :state="state"
     />
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import { useColorMode } from '@vueuse/core';
+import { defineComponent, PropType, ref } from 'vue';
+import Cookies from 'universal-cookie';
+import { usePreferredDark } from '@vueuse/core';
+
+const cookieName = 'vuetoolkit-color-scheme';
 
 export default defineComponent({
     name: 'ThemeSwitcher',
     props: {
+        domain: String,
         fontAwesomeStyle: {
             type: String,
             default: 'far'
@@ -30,34 +34,75 @@ export default defineComponent({
     computed: {
         faIcon(): string {
             let icon;
-            if (this.store === 'auto') {
+            if (this.state === 'auto') {
                 icon = 'tv';
-            } else if (this.store === 'light') {
+            } else if (this.state === 'light') {
                 icon = 'sun-bright';
-            } else if (this.store === 'dark') {
+            } else if (this.state === 'dark') {
                 icon = 'moon';
             }
             return `${this.fontAwesomeStyle} fa-fw fa-${icon}`;
         },
         label(): string {
-            return this.labels[this.store];
+            return this.labels[this.state as 'auto' | 'light' | 'dark'];
         }
     },
-    setup() {
-        const { store } = useColorMode();
-        return {
-            store
+    setup(props) {
+        let domain = props.domain;
+        if (domain) {
+            domain = domain.startsWith('.') ? domain : '.' + domain;
+        }
+        let date = new Date();
+        date.setFullYear(date.getFullYear() + 5);
+        let options = {
+            domain: domain,
+            expires: date
         };
+        let cookies = new Cookies(null, options);
+        let value = cookies.get(cookieName);
+        if (!['auto', 'light', 'dark'].includes(value)) {
+            cookies.set(cookieName, 'auto');
+            value = 'auto';
+        }
+        const state = ref(value);
+        return {
+            state: state,
+            cookies
+        };
+    },
+    created(): any {
+        this.setTheme();
     },
     methods: {
         next(): void {
-            if (this.store === 'auto') {
-                this.store = 'light';
-            } else if (this.store === 'light') {
-                this.store = 'dark';
-            } else if (this.store === 'dark') {
-                this.store = 'auto';
+            if (this.state === 'auto') {
+                this.state = 'light';
+            } else if (this.state === 'light') {
+                this.state = 'dark';
+            } else if (this.state === 'dark') {
+                this.state = 'auto';
             }
+            this.cookies.set(cookieName, this.state);
+            this.setTheme();
+        },
+        setTheme(): void {
+            if (this.state === 'dark') {
+                this.setDark();
+            } else if (this.state === 'light') {
+                this.setLight();
+            } else {
+                usePreferredDark().value
+                    ? this.setDark()
+                    : this.setLight();
+            }
+        },
+        setLight(): void {
+            document.documentElement.classList.add('light');
+            document.documentElement.classList.remove('dark');
+        },
+        setDark(): void {
+            document.documentElement.classList.add('dark');
+            document.documentElement.classList.remove('light');
         }
     }
 });
