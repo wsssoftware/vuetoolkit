8e846bd41af46fa6464bc6cf024b87c1c924ece5
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2025-06-11 14:50:03 -0300

    Export `GateService` from `vuetoolkit` to enhance service functionality.

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ export * from './src/Components/Form';
 export * from './src/Components/index';
 export * from './src/Composables/index';
 export * from './src/Directives/index';
+export { default as GateService } from './src/Gate';
 export { default as Plugin } from './src/Plugin';
