b7c2fa82b4065ff9cc38bfa1608ff2ab16ebe29e
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2025-07-01 17:35:40 -0300

    Remove `Link` component from `vuetoolkit` and update exports in Components index.

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,3 @@
 export { default as Gate } from './Gate.vue';
 export { default as Head } from './Head.vue';
-export { default as Link } from './Link.vue';
 export { default as ThemeSwitcher } from './ThemeSwitcher.vue';
